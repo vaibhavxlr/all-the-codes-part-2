5f0f540e8364581e0f933a5c623abd6df070be2b
Your Name <vaibhavxlr283@gmail.com>
2020-04-03 14:20:46 +0530

    complete codes

---
+++ Single_Number.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+     int x  = 0;
+     for(int i : nums){
+         x ^= i;
+     }
+     return x;
+}
+
+};
